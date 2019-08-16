ebd98a44d6de475239de7373e3a1661f8e9c8db4
arbex10 <lucasarbexmb@gmail.com>
2019-08-16 14:54:36 -0300

    1.0

--- database/seeds/UsersTableSeeder.php
+++ database/seeds/UsersTableSeeder.php
@@ -18,16 +18,6 @@ class UsersTableSeeder extends Seeder
         ]);
 
         User::create([
-            'name' => 'Anderson Martins',
-            'email' => 'anderson@gmail.com',
-            'username' => 'anderson',
-            'is_admin' => 0,
-            'is_coordinator' => 0,
-            'profile_id' => 2,
-            'password' => 'anderson'
-        ]);
-
-        User::create([
             'name' => 'Edson Yassudi',
             'email' => 'edson@core-sp.org.br',
             'username' => 'edson',
@@ -35,16 +25,6 @@ class UsersTableSeeder extends Seeder
             'is_coordinator' => 1,
             'profile_id' => 1,
             'password' => 'edson102030'
-        ]);
-
-        User::create([
-            'name' => 'Fernando',
-            'email' => 'fernando@core-sp.org.br',
-            'username' => 'fernando',
-            'is_admin' => 0,
-            'is_coordinator' => 1,
-            'profile_id' => 3,
-            'password' => 'fernando102030'
         ]);
     }
 }
