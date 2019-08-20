b501b86b3fd510a018723ad82dff03d4a0dea7a8
arbex10 <lucasarbexmb@gmail.com>
2019-08-20 11:53:48 -0300

    Simple correction

--- resources/views/profiles/tickets-completed.blade.php
+++ resources/views/profiles/tickets-completed.blade.php
@@ -43,7 +43,7 @@
                                     <td>{{ $ticket->id }}</td>
                                     <td><a href="{{ $ticket->path() }}">{{ $ticket->title }}</a></td>
                                     <td class="{{ bgPriority($ticket->priority) }}">{{ $ticket->priority }}</td>
-                                    <td>{{ $ticket->respondent->name }}</td>
+                                    <td>{!! isset($ticket->respondent->name) ? $ticket->respondent->name : '<i>Sem atribuição</i>' !!}</td>
                                     <td><strong class="text-muted">{{ dateAndHour($ticket->updated_at) }}</strong></td>
                                 </tr>
                             @empty
